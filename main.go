dadd209669b297cf6506407819e3ad293a5b8db9
Jacalz <Jacalz@users.noreply.github.com>
2024-02-21 13:53:52 +0100

    Initial dummy web server

--- main.go
+++ main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		return
+	}
+
+	fmt.Fprintln(w,
+		`<!DOCTYPE html>
+	<html>
+	<form method="post" action="/name">
+    <label for="firstname">First name:</label>
+    <input type="text" name="firstname" /><br />
+    <label for="lastname">Last name:</label>
+    <input type="text" name="lastname" /><br />
+    <input type="submit" />
+	</form></html>`)
+}
+
+func postHandler(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	name := r.Form.Get("firstname")
+	lastname := r.Form.Get("lastname")
+	fmt.Println(name, lastname)
+
+	fmt.Fprintln(w, "Form submitted fine")
+}
 
 func main() {
 	fmt.Println("Hello from BLHub server!")
+
+	http.HandleFunc("/index.html", helloHandler)
+	http.HandleFunc("/name", postHandler)
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
